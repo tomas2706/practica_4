ecf1f31a56fc8796332ae317a7e89075a332fad3
TheReal73 <gorka.sagri.novell7@gmail.com>
2024-12-09 10:08:24 +0100

    my_tee1

--- my_tee.c
+++ my_tee.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+	int fd = open(argv[1], O_CREAT, O_RDWR);
+	if (fd == -1) {
+        	perror("L'arxiu no s'ha pogut crear.");
+        	return 1;
+    	}
+	close(fd);
+}
